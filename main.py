127c0f8688cd39332380d691e97226d55e02a11a
Gábor Kovács <gabor950601@gmail.com>
2019-10-26 15:42:35 +0200

    working saving and loading for image proccessing

--- main.py
+++ main.py
@@ -2,30 +2,25 @@ import numpy as np
 import cv2
 #import time
 
-#file_name = 'C:/Users/csiga/Downloads/nokiavideos/pivideo3.mp4'
+file_name = 'C:/Users/csiga/Downloads/nokiavideos/pivideo3.mp4'
+#file_name = 'D:/codes/hackaton/nokia/output3.avi'
 
 #cap = cv2.VideoCapture(file_name)
 
 # time.sleep(2)
 
-imageMat = np.array((4, 5, 4), np.uint8)
+#imageMat = np.array((4, 5, 4), np.uint8)
 cap = cv2.VideoCapture(0)
 cap.open('http://192.168.0.190:8080/stream/video.mjpeg')
-cap.read(imageMat)
+#cap.read(imageMat)
 
 
 # Define the codec and create VideoWriter object
-
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#fourcc = cv2.cv.CV_FOURCC(*'X264')
-#out = cv2.VideoWriter('outputvideo4.avi', fourcc, 20.0, (640, 480))
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
-#out = cv2.VideoWriter('video.avi', -1, 25, (640, 480))
+out = cv2.VideoWriter('output7.avi',fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
 
 while True:
     ret, frame = cap.read()
-
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     out.write(frame)
 
