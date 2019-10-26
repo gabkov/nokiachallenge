5132371df8d28abe88af2d9e3f7a00bb6f5db052
Gábor Kovács <gabor950601@gmail.com>
2019-10-26 18:44:33 +0200

    display now one line thx bohi

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import cv2
 import time
 
 #file_name = 'C:/Users/csiga/Downloads/nokiavideos/pivideo3.mp4'
-#file_name = 'D:/codes/hackaton/nokia/output6.avi'
+file_name = 'D:/codes/hackaton/nokia/output11.avi'
 
-#cap = cv2.VideoCapture(file_name)
+cap = cv2.VideoCapture(file_name)
 
-time.sleep(2)
+#time.sleep(2)
 
 #imageMat = np.array((4, 5, 4), np.uint8)
-cap = cv2.VideoCapture(0)
-cap.open('http://192.168.0.190:8080/stream/video.mjpeg')
+#cap = cv2.VideoCapture(0)
+#cap.open('http://192.168.0.190:8080/stream/video.mjpeg')
 # cap.read(imageMat)
 
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output17.avi', fourcc, 20.0,(int(cap.get(3)), int(cap.get(4))))
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('output17.avi', fourcc, 20.0,(int(cap.get(3)), int(cap.get(4))))
 
 while True:
     ret, frame = cap.read()
@@ -48,16 +48,18 @@ while True:
     green_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
     green = cv2.bitwise_and(frame, frame, mask=green_mask)
 
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Red", red)
-    cv2.imshow("Blue", blue)
-    cv2.imshow("Yellow", green)
+    imageOut = np.hstack([frame, red, blue, green])
 
-    out.write(frame)
+    cv2.imshow("Frame", imageOut)
+    #cv2.imshow("Red", red)
+    #cv2.imshow("Blue", blue)
+    #cv2.imshow("Yellow", green)
+
+    #out.write(frame)
 
     if cv2.waitKey(50) & 0xFF == ord('q'):
         break
 
 cap.release()
-out.release()
+#out.release()
 cv2.destroyAllWindows()
