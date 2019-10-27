e72c44a1fe55cee258cd2b490c02b985634f4b3a
Gábor Kovács <gabor950601@gmail.com>
2019-10-27 04:14:56 +0100

    added cascade and artifical inteligenceeee diiik

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import time
 
 
 #file_name = 'C:/Users/csiga/Downloads/nokiavideos/pivideo3.mp4'
-file_name = 'D:/codes/hackaton/nokia/output5.avi'
+file_name = 'D:/codes/hackaton/nokia/output6.avi'
 
 cap = cv2.VideoCapture(file_name)
+
+#cap = cv2.VideoCapture(0)
 
 # time.sleep(2)
 
@@ -23,60 +25,25 @@ cap = cv2.VideoCapture(file_name)
 while True:
     ret, frame = cap.read()
 
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame = frame[80:300, 100:185]
 
-    # Red color
-    low_red = np.array([161, 155, 84])
-    high_red = np.array([179, 255, 255])
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
-    red = cv2.bitwise_and(frame, frame, mask=red_mask)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Blue color
-    low_blue = np.array([100, 50, 50])
-    high_blue = np.array([246, 96, 57])
-    blue_mask = cv2.inRange(hsv_frame, low_blue, high_blue)
-    blue = cv2.bitwise_and(frame, frame, mask=blue_mask)
+    lego_cascade = cv2.CascadeClassifier("cascade.xml")
 
-    # yellow color
-    low_yellow = np.array([25, 146, 190])
-    high_yellow = np.array([62, 174, 250])
-    yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-    yellow = cv2.bitwise_and(frame, frame, mask=yellow_mask)
 
-    # master color
-    low = np.array([152, 35, 12])
-    high = np.array([255, 255, 147])
-    mask = cv2.inRange(hsv_frame, low, high)
-    barmi = cv2.bitwise_and(frame, frame, mask=mask)
+    legos = lego_cascade.detectMultiScale(gray, scaleFactor=1.01, minNeighbors=50)
+    for(x, y, w, h) in legos:
+        print("x: {} y: {} w: {} h: {}".format(x, y, w, h))
+        frame = cv2.rectangle(frame, (x, y), (x+y, y+h), (0, 255, 0), 1)
 
-    # grey color
-    low_grey = np.array([103, 86, 65])
-    high_grey = np.array([145, 133, 128])
-    mask_grey = cv2.inRange(hsv_frame, low_grey, high_grey)
-    grey = cv2.bitwise_and(frame, frame, mask=mask_grey)
-
-    # create NumPy arrays from the boundaries
-    lower = np.array([103, 86, 65], dtype="uint8")
-    upper = np.array([145, 133, 128], dtype="uint8")
-
-    # find the colors within the specified boundaries and apply
-    # the mask
-    mask_grey2 = cv2.inRange(frame, lower, upper)
-    output = cv2.bitwise_and(frame, frame, mask=mask_grey2)
-
-    #imageOut = np.hstack([frame, red, blue, yellow])
     imageOut = np.hstack([frame])
 
-    cv2.rectangle(imageOut,(50,50),(250,240),(0,255,0),1)
-
     cv2.imshow("Frame", imageOut)
-    #cv2.imshow("Red", red)
-    #cv2.imshow("Blue", blue)
-    #cv2.imshow("Yellow", yellow)
 
     # out.write(frame)
 
-    if cv2.waitKey(50) & 0xFF == ord('q'):
+    if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
 cap.release()
